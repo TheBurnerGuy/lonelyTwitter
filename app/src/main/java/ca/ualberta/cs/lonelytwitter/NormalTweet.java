438baf4984c848a11b684135e6232b4849176726
TheBurnerGuy <TheBurnerGuy@users.noreply.github.com>
2017-01-17 18:12:52 -0700

    Create NormalTweet.java

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalTweet.java
@@ -0,0 +1,8 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+public class NormalTweet extends Tweet {
+  public NormalTweet(Date date, String message) throws TweetTooLongException{
+    super()
+  }
