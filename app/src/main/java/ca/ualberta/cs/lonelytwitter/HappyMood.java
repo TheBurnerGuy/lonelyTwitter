011bb1e34e3e7b22916cc4fd02b7e8bf003a359a
TheBurnerGuy <TheBurnerGuy@users.noreply.github.com>
2017-01-17 19:31:44 -0700

    Update HappyMood.java

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/HappyMood.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/HappyMood.java
@@ -1,8 +1,22 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import java.util.Date;
+
 /**
  * Created by henrywei on 1/17/17.
  */
 
 public class HappyMood extends Mood {
+    public HappyMood(Date date) {
+        super(date);
+    }
+
+    public HappyMood() {
+        super();
+    }
+
+    @Override
+    public String getMood() {
+        return ":)";
+    }
 }
