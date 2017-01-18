7515523bd4b980a68bbfbdf58c51f8130980feec
TheBurnerGuy <TheBurnerGuy@users.noreply.github.com>
2017-01-17 18:03:43 -0700

    Create ImportantTweet.java

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ImportantTweet.java
@@ -0,0 +1,13 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+public class ImportantTweet extends Tweet{
+  public ImportantTweet(String message) throws TweetTooLongException{
+    super(message);
+  }
+  
+  public ImportantTweet(Date date, String message) throws TweetTooLongException{
+    //yarr
+  }
+}
