fdbb72981eecbe7e1eb8e5d518c256aacda23c5c
TheBurnerGuy <TheBurnerGuy@users.noreply.github.com>
2017-01-17 19:32:09 -0700

    Update SadMood.java

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/SadMood.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/SadMood.java
@@ -1,8 +1,22 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import java.util.Date;
+
 /**
  * Created by henrywei on 1/17/17.
  */
 
 public class SadMood extends Mood {
+    public SadMood(Date date) {
+        super(date);
+    }
+
+    public SadMood() {
+        super();
+    }
+
+    @Override
+    public String getMood() {
+        return ":(";
+    }
 }
