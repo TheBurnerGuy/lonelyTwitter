5df22351e57224e7005efa4a90e59f98f4721c89
TheBurnerGuy <henryweilo@hotmail.com>
2017-01-24 18:35:30 -0700

    help me

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -1,41 +1,48 @@
 package ca.ualberta.cs.lonelytwitter;
 
-import java.util.ArrayList;
 import java.util.Date;
 
-public astract class Tweet implements Tweetable{
-  private Date date;
-  private String message;
-  private ArrayList moodList = new ArrayList<Mood>();
-  
-  public Tweet(Date date, String message) throws TweetTooLongException{
-    this.date = date;
-    this.setMessage(message);
-  }
-  public Tweet(String message) throws TweetTooLongException{
-    this.setMessage(message);
-    this.date = new Date();
-  }
-  
-  public abstract Boolean isImportant();
-  
-  public Date getDate(){
-   return this.date; 
-  }
-  
-  public void setDate(Date date){
-   this.date = date; 
-  }
-  
-  public String getMessage(){
-   return message; 
-  }
-  
-  public Date setMessage(String message) throws TweetTooLongException{
-    if(message.length() > 144){
-      throw new TweetTooLongException();
-    }else{
-      this.message = message;
+/**
+ * Created by shida3 on 1/19/17.
+ */
+
+public abstract class Tweet implements Tweetable{
+    private Date date;
+    private String message;
+
+    public Tweet(String message){
+        this.message = message;
+        this.date = new Date();
     }
-  }
+
+    public Tweet(Date date, String message){
+        this.message = message;
+        this.date = date;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) throws TweetTooLongException{
+        if (message.length() > 140){
+            throw new TweetTooLongException();
+        }
+        this.message = message;
+    }
+
+    public abstract Boolean isImportant();
+
+    @Override
+    public String toString(){
+        return date.toString() + " | " + message;
+    }
 }
