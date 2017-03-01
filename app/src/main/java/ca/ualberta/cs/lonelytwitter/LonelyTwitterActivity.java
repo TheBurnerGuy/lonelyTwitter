c1bbf9bef1b50c3049c4fd8d789ac93c1f8015b5
TheBurnerGuy <TheBurnerGuy@users.noreply.github.com>
2017-02-28 18:36:57 -0700

    Update LonelyTwitterActivity.java

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -1,7 +1,6 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -15,62 +14,29 @@ import java.util.Date;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import static android.widget.TextView.BufferType.EDITABLE;
-
-/**
- * This class is the main view class of the project. <br/> In this class, user interaction
- * and file manipulation is performed.
- * All files are in the form of *json* files that are stored in Emulator's' accessible from Android Device Monitor:
- * <pre>
- *     pre-formatted text: <br>
- *         File Explorer -> data -> data -> ca.ualberta.cs.lonelytwitter -> files -> file.sav
- * </pre>
- * <code> begin <br>
- * some pseudo code <br>
- * end.</code>
- * The file name is indicated in the &nbsp &nbsp &nbsp FILENAME constant.
- * <ul>
- *     <li>item 1</li>
- *     <li>item 2</li>
- *     <li>item 3</li>
- * </ul>
- * <ol>
- *     <li>item 1</li>
- *     <li>item 2</li>
- *     <li>item 3</li>
- * </ol>
- *
- * @author henrywei
- * @version 1.0
- * @see Tweet
- * @since 0.5
- */
 public class LonelyTwitterActivity extends Activity {
-	/**
-	 * The file that all the tweets are saved there. The format of the file is JSON.
-	 * @see #loadFromFile()
-	 * @see #saveInFile()
-	 */
+	private LonelyTwitterActivity activity = this;
+
 	private static final String FILENAME = "file.sav";
-	private enum TweetListOrdering {DATE_ASCENDING, DATE_DESCENDING, TEXT_ASCENDING, TEXT_DESCENDING};
 	private EditText bodyText;
 	private ListView oldTweetsList;
-
-	private ArrayList<Tweet> tweetList;
+	private ArrayList<Tweet> tweetList = new ArrayList<Tweet>();
 	private ArrayAdapter<Tweet> adapter;
 
-	/** Called when the activity is first created. */
+	public ListView getOldTweetsList(){
+		return oldTweetsList;
+	}
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -86,15 +52,10 @@ public class LonelyTwitterActivity extends Activity {
 			public void onClick(View v) {
 				setResult(RESULT_OK);
 				String text = bodyText.getText().toString();
-				text = trimExtraSpaces(text);
-
-				Tweet tweet = new NormalTweet(text);
-				tweetList.add(tweet);
-
+				Tweet newTweet = new NormalTweet(text);
+				tweetList.add(newTweet);
 				adapter.notifyDataSetChanged();
-
 				saveInFile();
-
 			}
 		});
 
@@ -102,66 +63,40 @@ public class LonelyTwitterActivity extends Activity {
 
 			public void onClick(View v) {
 				setResult(RESULT_OK);
-
 				tweetList.clear();
-
+				deleteFile("file.sav");
 				adapter.notifyDataSetChanged();
-
-				saveInFile();
-
 			}
 		});
-
 	}
+
+	oldTweetsList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+		public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
+			Intent intent = new Intent(activity, EditTweetActivity.class);
+			intent.putExtra("message", adapter.getItem(i).getMessage());
+			startActivity(intent);
+		}
+	});
 
 	@Override
 	protected void onStart() {
 		// TODO Auto-generated method stub
 		super.onStart();
-
 		loadFromFile();
-
 		adapter = new ArrayAdapter<Tweet>(this,
 				R.layout.list_item, tweetList);
 		oldTweetsList.setAdapter(adapter);
 	}
 
-	/**
-	 * Trims extra spaces using regular expression.
-	 * @param inputString string that needs to be cleared of extra spaces
-	 * @return resulting String
-     */
-	private String trimExtraSpaces(String inputString){
-		inputString = inputString.replaceAll("\\s+", " ");
-		return inputString;
-	}
 
-	/**
-	 * This method sorts items in the tweet list and refreshes the adater.
-	 * @param ordering ordering to be used
-     */
-	private void sortTweetListItems(TweetListOrdering ordering){
-
-	}
-
-	/**
-	 * Loads tweets from specified file.
-	 *
-	 * @throws TweetTooLongException if the text is too long
-	 * @exception FileNotFoundException if the file is not created first.
-	 */
 	private void loadFromFile() {
 		try {
 			FileInputStream fis = openFileInput(FILENAME);
 			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-
 			Gson gson = new Gson();
-
-			// Taken from http://stackoverflow.com/questions/12384064/gson-convert-from-json-to-a-typed-arraylistt
-			// 2017-01-24 18:19
+			//Code taken from http://stackoverflow.com/questions/12384064/gson-convert-from-json-to-a-typed-arraylistt Sept.22,2016
 			Type listType = new TypeToken<ArrayList<NormalTweet>>(){}.getType();
 			tweetList = gson.fromJson(in, listType);
-
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			tweetList = new ArrayList<Tweet>();
@@ -171,21 +106,15 @@ public class LonelyTwitterActivity extends Activity {
 		}
 	}
 
-	/**
-	 * Saves tweets to a specified file in JSON format.
-	 * @throws FileNotFoundException if file folder doesn't exist
-	 */
+
 	private void saveInFile() {
 		try {
-			FileOutputStream fos = openFileOutput(FILENAME,
-					Context.MODE_PRIVATE);
-			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
 
+			FileOutputStream fos = openFileOutput(FILENAME,0);
+			OutputStreamWriter writer = new OutputStreamWriter(fos);
 			Gson gson = new Gson();
-			gson.toJson(tweetList, out);
-			out.flush();
-
-			fos.close();
+			gson.toJson(tweetList, writer);
+			writer.flush();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			throw new RuntimeException();
